f211052a09b6cbec87bd980589abf75adfd97a1b
prabanjan127 <prabanjansakthibabu@gmail.com>
2024-01-26 00:54:30 +0530

    date added

--- src/components/ToDoForm.js
+++ src/components/ToDoForm.js
@@ -3,13 +3,15 @@ import { TodoContext } from './ToDoApp';
 
 const TodoForm = () => {
   const { addTodo } = useContext(TodoContext);
-  const [task, setTask] = useState('');
 
+  const [task, setTask] = useState('');
+  const [date,setDate]=useState('');
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (task.trim() !== '') {
-      addTodo(task);
+    if (task.trim() !== '' && date.trim() !=='') {
+      addTodo(task,date);
       setTask('');
+      setDate('');
     }
   };
 
@@ -21,7 +23,15 @@ const TodoForm = () => {
         value={task}
         onChange={(e) => setTask(e.target.value)}
       />
-      <button type="submit">Add Task</button>
+      <input
+        type="date"
+        placeholder="Enter a new task"
+        value={date}
+        onChange={(e) => setDate(e.target.value)}
+        required
+      />
+       <button type="submit">Add Task</button>
+     
     </form>
   );
 };
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,18 +2,22 @@ import React, { useContext } from 'react';
 import { TodoContext } from './ToDoApp';
 
 const TodoList = () => {
-  const { todos, removeTodo } = useContext(TodoContext);
+  const { todos,addDate, removeTodo } = useContext(TodoContext);
 
   return (
     <ul>
       {todos.map((todo) => (
         <li key={todo.id}>
-          {todo.task}
+          {todo.task} : {getTaskDate(todo.id)}
           <button onClick={() => removeTodo(todo.id)}>Remove</button>
         </li>
       ))}
     </ul>
   );
+  function getTaskDate(id) {
+    const dateEntry = addDate.find((entry) => entry.id === id);
+    return dateEntry ? dateEntry.date : 'No Date';
+  }
 };
 
 export default TodoList;
